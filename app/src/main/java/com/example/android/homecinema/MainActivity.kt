5194c2828e26313895bfbe828dd95c9c840db2ab
Joonas Leonsaari <joonas.leonsaari@gmail.com>
2018-10-01 13:21:38 +0300

    added animation after splashscreen disappears & applied app colors + themes into colors.xml + styles.xml

--- app/src/main/java/com/example/android/homecinema/MainActivity.kt
+++ app/src/main/java/com/example/android/homecinema/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.android.homecinema
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.transition.Fade
+import android.transition.TransitionManager
+import android.view.ViewGroup
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onStart() {
+
+        val mRootView: ViewGroup = findViewById(R.id.homeScreen)
+        val mFade: Fade = Fade(Fade.IN)
+        TransitionManager.beginDelayedTransition(mRootView, mFade)
+
+        super.onStart()
     }
 }
--- app/src/main/java/com/example/android/homecinema/SplashActivity.kt
+++ app/src/main/java/com/example/android/homecinema/SplashActivity.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.transition.Fade
+import android.transition.TransitionManager
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 
 class SplashActivity : AppCompatActivity() {
     private var mDelayHandler: Handler? = null
@@ -14,6 +18,7 @@ class SplashActivity : AppCompatActivity() {
 
             val intent = Intent(applicationContext, MainActivity::class.java)
             startActivity(intent)
+            overridePendingTransition(R.transition.fade_in, R.transition.fade_out)
             finish()
         }
     }
@@ -21,6 +26,7 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        overridePendingTransition(R.transition.fade_in, R.transition.fade_out)
 
         //Initialize the Handler
         mDelayHandler = Handler()
@@ -28,6 +34,11 @@ class SplashActivity : AppCompatActivity() {
         //Navigate with delay
         mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
 
+    }
+
+    override fun onStart() {
+        overridePendingTransition(R.transition.fade_in, R.transition.fade_out)
+        super.onStart()
     }
 
     public override fun onDestroy() {
